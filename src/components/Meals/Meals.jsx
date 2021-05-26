198b87bd9c2f3c35052ce5f1e457b4aa7846d965
Pedro Matias <pedrollmatias@gmail.com>
2021-05-26 15:57:38 -0300

    feat: includes meals component structure

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import MealsSummary from './MealsSummary';
+import AvaliableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaliableMeals />
+    </>
+  );
+};
+
+export default Meals;
